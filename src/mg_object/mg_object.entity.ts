861abb307bda1d4167356a94bd0f324bb1f00692
kwaktaemin <kwak9898@github.com>
2022-08-29 18:24:35 +0900

    카멜케이스로 전환

--- src/mg_object/mg_object.entity.ts
+++ src/mg_object/mg_object.entity.ts
@@ -3,35 +3,35 @@ import {Entity, PrimaryColumn, Column} from "typeorm";
 @Entity()
 export class MgObjectEntity {
     @PrimaryColumn({type: "varchar", comment: "사물 고유값"})
-    mg_id: string
+    mgId: string
 
     @Column({type: "varchar", comment: "사물 그룹 아이디"})
-    mg_group_id: string
+    mgGroupId: string
 
     @Column({type: "varchar", comment: "사물 이름"})
-    mg_name: string
+    mgName: string
 
     @Column({type: "smallint", comment: "상태값", default: 0})
-    status_flag: number
+    statusFlag: number
 
     @Column({type: "timestamptz", comment: "생성일", default: () => "CURRENT_TIMESTAMP"})
-    created_at: string
+    createdAt: string
 
     @Column({type: "timestamptz", comment: "수정일", default: () => "CURRENT_TIMESTAMP"})
-    updated_at: string
+    updatedAt: string
 
     @Column({type: "timestamptz", comment: "삭제일"})
-    deleted_at: string
+    deletedAt: string
 
     @Column({type: "varchar", comment: "사물 분류"})
-    mg_category: string
+    mgCategory: string
 
     @Column({type: "bigint", comment: "좋아요 개수", default: 0})
-    like_cnt: number
+    likeCnt: number
 
     @Column({type: "int", comment: "좋아요 순위", default: 9999})
-    like_rank: number
+    likeRank: number
 
     @Column({type: "int", comment: "순위 변동", default: 0})
-    rank_change: number
+    rankChange: number
 }
