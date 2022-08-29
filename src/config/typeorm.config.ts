d4f30e07467806fa6b4015ea81b1488e1fd637ae
kwaktaemin <kwak9898@github.com>
2022-08-29 14:10:45 +0900

    typeorm config settings

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,11 +4,11 @@ import { UserEntity } from "../user/user.entity";
 export const typeormConfig: TypeOrmModuleOptions = {
     // DataBase Type
     type : 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'oathbreaker',
-    password: 'Merge135!#%',
-    database: 'oathbreaker',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [UserEntity],
     synchronize: true
 }
