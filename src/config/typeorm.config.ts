5080304a286d022fe2cc20c93d1b394b60f0f765
kwaktaemin <kwak9898@github.com>
2022-08-29 16:42:46 +0900

    DB Type MySQL -> Postgres

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,7 +3,7 @@ import { UserEntity } from "../user/user.entity";
 
 export const typeormConfig: TypeOrmModuleOptions = {
     // DataBase Type
-    type : 'mysql',
+    type : 'postgres',
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
     username: process.env.DB_USERNAME,
