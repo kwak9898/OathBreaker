6f136fa3f06520f9dd3c2c805472893f309d0801
kwaktaemin <kwak9898@github.com>
2022-08-29 18:23:45 +0900

    카멜케이스로 전환

--- src/mg_image/mg_image.entity.ts
+++ src/mg_image/mg_image.entity.ts
@@ -3,34 +3,34 @@ import {Entity, PrimaryColumn, Column} from "typeorm";
 @Entity()
 export class MgImageEntity {
     @PrimaryColumn({type: "varchar", comment: "이미지 고유값", nullable: false})
-    img_id: string;
+    imgId: string;
 
     @Column({type: "varchar", comment: "사물 고유값"})
-    mg_id: string;
+    mgId: string;
 
     @Column({type: "varchar", comment: "이미지 파일명"})
-    img_name: string;
+    imgName: string;
 
     @Column({type: "varchar", comment: "이미지 url"})
-    img_url: string;
+    imgUrl: string;
 
     @Column({type: "varchar", comment: "이미지 s3 key"})
-    s3_key: string;
+    s3Key: string;
 
     @Column({type: "smallint", comment: "사물 이미지 상태값", default: 1})
-    status_flag: number;
+    statusFlag: number;
 
     @Column({type: "timestamptz", comment: "생성일", default: () => "CURRENT_TIMESTAMP"})
-    created_at: string;
+    createdAt: string;
 
     @Column({type: "timestamptz", comment: "수정일", default: () => "CURRENT_TIMESTAMP"})
-    updated_at: string;
+    updatedAt: string;
 
     @Column({type: "timestamptz", comment: "삭제일"})
-    deleted_at: string;
+    deletedAt: string;
 
     @Column({type: "varchar", comment: "등록자 타입: MANAGER_USER, MERGE_USER", default: "MERGE_USER"})
-    user_type: string;
+    userType: string;
 
     @Column({type: "double", comment: "위도(37.56667)", default: 0})
     latitude: number;
@@ -39,41 +39,41 @@ export class MgImageEntity {
     longitube: number;
 
     @Column({type: "varchar", comment: "mask origin name"})
-    mask_name: string;
+    maskName: string;
 
     @Column({type: "varchar", comment: "mask s3 키"})
-    s3_key_mask: string;
+    s3KeyMask: string;
 
     @Column({type: "varchar", comment: "sallency origin name"})
-    sallency_name: string;
+    sallencyName: string;
 
     @Column({type: "varchar", comment: "sallency s3 키"})
-    s3_key_sallency: string;
+    s3KeySallency: string;
 
     @Column({type: "smallint", comment: "depth_camera(Lidar) 사용 여부 미사용: 0iOS Lidar 사용: 1", default: 1})
-    is_depth_camera: number;
+    isDepthCamera: number;
 
     @Column({type: "jsonb", comment: "물체 너비 / 폭 / 높이 / 형태, 환경코드, 중앙점 거리값, 가속 X/Y/Z{    \"objectWidth\": 0.1240021,    \"objectLength\": 0.1285654,    \"objectHeight\": 0.1689597,    \"objectShape\": \"Ellipse\",    \"envCode\": \"Object_On_Horizontal_Plane\",    \"centerDepth\": 524.2222,    \"accelX\": 0.04507446,    \"accelY\": -0.8758545,    \"accelZ\": -0.4858856,}"})
-    meta_data: string;
+    metaData: string;
 
     @Column({type: "varchar", comment: "depth origin 파일명"})
-    depth_name: string;
+    depthName: string;
 
     @Column({type: "varchar", comment: "depth s3 키"})
-    s3_key_depth: string;
+    s3KeyDepth: string;
 
     @Column({type: "varchar", comment: "confidence origin 파일명"})
-    confidence_name: string;
+    confidenceName: string;
 
     @Column({type: "varchar", comment: "confidence s3 키"})
-    s3_key_confidence: string;
+    s3KeyConfidence: string;
 
     @Column({type: "varchar", comment: "회원 아이디"})
-    user_id: string;
+    userId: string;
 
     @Column({type: "varchar", comment: "사람이 직접 입력한 옳은 이름"})
-    correct_name: string;
+    correctName: string;
 
     @Column({type: "varchar", comment: "머지웨어에서 생성된 Crab 이미지"})
-    crop_img_url: string;
+    cropImgUrl: string;
 }
