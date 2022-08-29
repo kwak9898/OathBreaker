63c447371fb3daca681a03ab231300dcf60809db
kwaktaemin <kwak9898@github.com>
2022-08-29 18:16:02 +0900

    MgObject Entity 수정

--- src/mg_object/mg_object.entity.ts
+++ src/mg_object/mg_object.entity.ts
@@ -11,30 +11,27 @@ export class MgObjectEntity {
     @Column({type: "varchar", comment: "사물 이름"})
     mg_name: string
 
-    @Column({type: "smallint", comment: "상태값"})
+    @Column({type: "smallint", comment: "상태값", default: 0})
     status_flag: number
 
-    @Column({type: "timestamptz", comment: "생성일"})
+    @Column({type: "timestamptz", comment: "생성일", default: () => "CURRENT_TIMESTAMP"})
     created_at: string
 
-    @Column({type: "timestamptz", comment: "수정일"})
+    @Column({type: "timestamptz", comment: "수정일", default: () => "CURRENT_TIMESTAMP"})
     updated_at: string
 
     @Column({type: "timestamptz", comment: "삭제일"})
     deleted_at: string
 
-    @Column({type: "varchar", comment: "언제 사용하나"})
-    when_use: string
-
     @Column({type: "varchar", comment: "사물 분류"})
     mg_category: string
 
-    @Column({type: "bigint", comment: "좋아요 개수"})
+    @Column({type: "bigint", comment: "좋아요 개수", default: 0})
     like_cnt: number
 
-    @Column({type: "int", comment: "좋아요 순위"})
+    @Column({type: "int", comment: "좋아요 순위", default: 9999})
     like_rank: number
 
-    @Column({type: "int", comment: "순위 변동"})
+    @Column({type: "int", comment: "순위 변동", default: 0})
     rank_change: number
 }
