cf6a7d376a475fbdf379eb5792dd0bcce7a33765
kwaktaemin <kwak9898@github.com>
2022-08-29 14:25:44 +0900

    dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { UserModule } from "./user/user.module";
+import { UserModule } from './user/user.module';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({
+  path: path.resolve(
+      (process.env.NODE_ENV === 'production') ? '.production.env'
+          : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env'
+  )
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
