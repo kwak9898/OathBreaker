b8222f6e52f0725b3c96b1e3a18f7ca988b4cc7e
kwaktaemin <kwak9898@github.com>
2022-08-30 18:35:53 +0900

    TypeOrm Config 설정

---
+++ src/config/database/typeorm.service.ts
@@ -0,0 +1,24 @@
+import {TypeOrmModuleOptions, TypeOrmOptionsFactory} from '@nestjs/typeorm';
+import {Injectable} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+import { MgoImage } from "../../entity/mgoImage.entity";
+import { MgObject } from "../../entity/mgObject.entity";
+
+@Injectable()
+export class TypeormService implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService) {};
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type : 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [MgoImage, MgObject],
+            synchronize: false,
+            logging: true
+        }
+    }
+}
