8815ddbdfd3a4e03a6a9cdb5cf0c7738b7ecfea2
kwaktaemin <kwak9898@github.com>
2022-08-30 11:49:56 +0900

    OathUserModule 추가

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { UserModule } from './user/user.module';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
+import { NestFactory } from "@nestjs/core";
+import {OathuserModule} from "./oathuser/oathuser.module";
+import * as dotenv from "dotenv";
+import * as path from "path";
 
 dotenv.config({
   path: path.resolve(
-      (process.env.NODE_ENV === 'production') ? '.production.env'
-          : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env'
+      (process.env.NODE_ENV === "production") ? ".production.env"
+          : (process.env.NODE_ENV === "stage") ? ".stage.env" : ".development.env"
   )
 });
 
 async function bootstrap() {
-  const app = await NestFactory.create(UserModule);
+  const app = await NestFactory.create(OathuserModule);
   await app.listen(3000);
 }
+
 bootstrap();
