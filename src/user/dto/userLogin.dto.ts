9833434706daa2b6560389ece855a400219ec868
kwaktaemin <kwak9898@github.com>
2022-08-31 18:28:20 +0900

    userDto Validation 추가

--- src/user/dto/userLogin.dto.ts
+++ src/user/dto/userLogin.dto.ts
@@ -1,9 +1,14 @@
-import {IsNotEmpty} from "class-validator";
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class UserLoginDto {
     @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(30)
     userId: string;
 
     @IsNotEmpty()
+    @IsString()
+    @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
     password: string;
 }
