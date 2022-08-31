7511c2f8001843930b99d70b82c48a13c1fe1645
kwaktaemin <kwak9898@github.com>
2022-08-31 18:22:05 +0900

    로그인 구현 완료

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,26 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {Body, Controller, Param, Patch, Post, Req, UseGuards} from '@nestjs/common';
 import { UserService } from "./user.service";
 import { Request } from "express";
 import {UserLoginDto} from "./dto/userLogin.dto";
+import {AuthGuard} from "../guard/auth.guard";
 
+@UseGuards(AuthGuard)
 @Controller('oath-user')
 export class UserController {
-    constructor(private oathUserService: UserService) {}
+    constructor(private userService: UserService) {}
 
+    // 회원 Login Controller
+    @UseGuards(AuthGuard)
     @Post('/login')
     async login(@Body() userLoginDto: UserLoginDto) {
-        const { userId } = userLoginDto;
-        return await this.oathUserService.checkUserExist(userId)
+        const { userId, password } = userLoginDto;
+        return await this.userService.login(userId, password);
+    }
+
+    // 회원 Password 변경
+    @Patch()
+    async updateByUserPassword(@Param() userLoginDto: UserLoginDto) {
+        const { userId, password } = userLoginDto;
+        return await this.userService.checkUserExist(userId)
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,25 +1,37 @@
-import { HttpException, HttpStatus, Injectable, UnprocessableEntityException } from '@nestjs/common';
+import {
+    HttpException,
+    HttpStatus,
+    Inject,
+    Injectable,
+    NotFoundException,
+    UnprocessableEntityException
+} from '@nestjs/common';
 import { InjectRepository } from "@nestjs/typeorm";
-import { user } from "../entity/user.entity";
+import { User } from "../entity/user.entity";
 import { Connection, Repository } from "typeorm";
 import { UserLoginDto } from "./dto/userLogin.dto";
 import { JwtService } from "@nestjs/jwt";
 import * as bcrypt from "bcrypt";
+import { ConfigType } from "@nestjs/config";
+import * as jwt from "jsonwebtoken";
 
 @Injectable()
 export class UserService {
     constructor(
-        @InjectRepository(user)
-        private oathUserRepository: Repository<user>, jwtService: JwtService, connection: Connection
+        @InjectRepository(User)
+        private userRepository: Repository<User>, jwtService: JwtService, connection: Connection,
+
+        @Inject(authConfig.KEY)
+        private config: ConfigType<typeof authConfig>
     ) {
     }
 
     // 존재하는 유저 확인
-    async checkUserExist (userId: string): Promise<user> {
+    async checkUserExist (userId: string): Promise<User> {
         // TODO 로그인 API 구현하기
         // 1. userId, password를 가진 유저가 존재하는지 DB에서 확인 후 없다면 에러 처리하기.
         // 2. JWT 발급하기.
-        const user = await this.oathUserRepository.findOne({where: {userId: userId}})
+        const user = await this.userRepository.findOne({where: {userId: userId}})
 
         if (user) {
             return user
@@ -37,6 +49,31 @@ export class UserService {
 
         if (existUser) {
             throw new UnprocessableEntityException("해당 아이디로는 가입할 수 없습니다.")
+        }
+    }
+
+    // JWT 발급
+    private async getByJwt(user: UserLoginDto) {
+        const payload = { ...user }
+
+        return jwt.sign(payload, this.config.jwtSecret, {
+            expiresIn: "1d",
+            audience: "exampleId",
+            issuer: "exampleId"
+        })
+    }
+
+    // 회원 로그인
+    async login(userID: string, password: string): Promise<string> {
+        const user = await this.userRepository.findOne({where: {userId: userID, password: password}});
+
+        if (!user) {
+            throw new NotFoundException("유저가 존재하지 않습니다.");
+        } else {
+            return this.getByJwt({
+                userId: user.userId,
+                password: user.password
+            })
         }
     }
 }
