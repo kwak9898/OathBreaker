ef6f13aa6cc2396576cc45f49eec1ea27eb88b55
kwaktaemin <kwak9898@github.com>
2022-08-31 12:33:49 +0900

    로그인 API 코드 초기화 및 다시 시작

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -16,38 +16,10 @@ export class UserService {
 
     // 존재하는 유저 로그인
     async existUserLogin (userId: string, password: string): Promise<OathUser> {
-        try {
-            const user = await this.oathUserRepository.findOne({select: ["password"], where: {userId: userId}});
-            const isPasswordMatching = await bcrypt.compare(
-                password,
-                user.password
-            );
+        // TODO 로그인 API 구현하기
+        // 1. userId, password를 가진 유저가 존재하는지 DB에서 확인 후 없다면 에러 처리하기.
+        // 2. JWT 발급하기.
 
-            if (!isPasswordMatching) {
-                throw new HttpException(
-                    "잘못된 인증 정보입니다.",
-                    HttpStatus.BAD_REQUEST,
-                );
-            }
-            user.password = undefined;
-
-            return user
-        } catch (err) {
-            throw new HttpException(
-                "잘못된 경로입니다.",
-                HttpStatus.BAD_REQUEST,
-            )
-        }
+        return
     }
 }
-
-// const user = await this.oathUserRepository.findOne({select: ['password'], where: {userId: userId}})
-//
-// if (user) {
-//     return user;
-// } else {
-//     throw new HttpException(
-//         "존재하지 않은 아이디입니다.",
-//         HttpStatus.NOT_FOUND
-//     );
-// }
