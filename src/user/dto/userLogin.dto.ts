31fd0e16a8a2c7b42e8a72e3554fc3a994674486
kwaktaemin <kwak9898@github.com>
2022-08-31 18:34:58 +0900

    ID 공백 처리 및 Password, Id 문자열 불일치 처리

--- src/user/dto/userLogin.dto.ts
+++ src/user/dto/userLogin.dto.ts
@@ -1,12 +1,22 @@
 import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { Transform } from "class-transformer";
+import {BadRequestException} from "@nestjs/common";
 
 export class UserLoginDto {
+    @Transform(params => params.value.trim())
     @IsNotEmpty()
     @IsString()
     @MinLength(2)
     @MaxLength(30)
     userId: string;
 
+    @Transform(({ value, obj }) => {
+        if (obj.password.includes(value.trim())) {
+            throw new BadRequestException("password는 ID와 같은 문자열을 포함할 수 없습니다.")
+        }
+
+        return value.trim();
+    })
     @IsNotEmpty()
     @IsString()
     @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
