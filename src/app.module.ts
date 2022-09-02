04b0167292c9a8b34c430b070712baa82a278eef
kwaktaemin <kwak9898@github.com>
2022-09-02 14:33:37 +0900

    JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,16 @@ import { TypeormService } from "./config/database/typeorm.service";
 import { TypeormModule } from "./config/database/typeorm.module";
 import { LoggerMiddleware } from "./middleware/logger.middleware";
 import { UserController } from "./user/user.controller";
+import * as Joi from "joi";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JET_EXPIRATION_TIME: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRootAsync({
       imports: [TypeormModule],
       useClass: TypeormService,
