448a216fdfa3b4293d8ebfc9c8f17bedaa7759c4
kwak9898 <kwak9898@github.com>
2022-09-06 17:26:45 +0900

    roles 세팅 중

--- src/models/roles/roles.module.ts
+++ src/models/roles/roles.module.ts
@@ -1,9 +1,34 @@
-import { Module } from '@nestjs/common';
-import { RolesService } from './roles.service';
-import { RolesController } from './roles.controller';
+import { Module } from "@nestjs/common";
+import { RolesService } from "./roles.service";
+import { RolesController } from "./roles.controller";
+import { UsersModule } from "../users/users.module";
+import { PassportModule } from "@nestjs/passport";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { LocalStrategy } from "../../strategies/local.strategy";
+import { JwtStrategy } from "../../strategies/jwt.strategy";
+import { JwtRefreshStrategy } from "../../strategies/jwtRefresh.strategy";
 
 @Module({
-  providers: [RolesService],
-  controllers: [RolesController]
+  imports: [
+    UsersModule,
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get("JWT_ACCESS_TOKEN_SECRET"),
+        signOptions: {
+          expiresIn: `${configService.get(
+            "JWT_ACCESS_TOKEN_EXPIRATION_TIME"
+          )}s`,
+        },
+      }),
+    }),
+  ],
+  providers: [RolesService, LocalStrategy, JwtStrategy, JwtRefreshStrategy],
+  exports: [RolesService, JwtModule],
+  controllers: [RolesController],
 })
 export class RolesModule {}
