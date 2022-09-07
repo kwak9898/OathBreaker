53972dc5bd08066a55f0b160ec84ac63a7f20b97
kwaktaemin <kwak9898@github.com>
2022-09-07 11:20:13 +0900

    역할 생성 API

--- src/models/roles/roles.controller.ts
+++ src/models/roles/roles.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
+import { Response } from "express";
+import { AuthService } from "../auth/auth.service";
+import { UsersService } from "../users/users.service";
+import { RolesService } from "./roles.service";
+import { Public } from "../../dacorators/skipAuth.decorator";
+import { LocalAuthGuard } from "../../guards/auth/localAuth.guard";
 
 @Controller("roles")
-export class RolesController {}
+export class RolesController {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+    private rolesService: RolesService
+  ) {}
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post("create-user")
+  async createRole(@Req() req, @Res({ passthrough: true }) res: Response) {
+    const user = req.user;
+    const { accessToken, ...accssOption } =
+      this.authService.getCookieWithJwtAccessToken(user.userId);
+
+    const { refreshToken, ...refreshOption } =
+      this.authService.getCookieWithJwtRefreshToken(user.userId);
+
+    await this.usersService.setCurrentRefreshToken(refreshToken, user.userId);
+    await this.rolesService.createRoles(user.roleName, user.userId);
+
+    res.cookie("Authentication", accessToken, accssOption);
+    res.cookie("Refresh", refreshToken, refreshOption);
+  }
+}
