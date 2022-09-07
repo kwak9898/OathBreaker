26daab8e828cfe732a453db29eabdbcbda906cff
kwaktaemin <kwak9898@github.com>
2022-09-07 09:59:25 +0900

    역할 관련 API

--- src/models/roles/roles.service.ts
+++ src/models/roles/roles.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "../../database/entities/user.entity";
+import { UsersService } from "../users/users.service";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private usersService: UsersService,
+    private authService: AuthService
+  ) {}
+
+  // 관리자인지 등록자인지의 역할 관련 함수
+  async rolesName() {
+    return;
+  }
+}
