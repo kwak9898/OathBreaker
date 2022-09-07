0a40ca009dec29e49e43df308ab1992362881559
kwaktaemin <kwak9898@github.com>
2022-09-07 15:09:53 +0900

    Roles Module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { JwtAuthGuard } from "./guards/auth/jwtAuth.guard";
 import { DatabaseModule } from "./database/database.module";
 import { ConfigModule } from "@nestjs/config";
 import * as Joi from "@hapi/joi";
+import { RolesModule } from "./models/roles/roles.module";
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import * as Joi from "@hapi/joi";
     }),
     UsersModule,
     AuthModule,
+    RolesModule,
     DatabaseModule,
   ],
   controllers: [AppController],
