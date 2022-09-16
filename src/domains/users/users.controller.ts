006a7753b6dcb430df223f83d440d91ddf327495
kwaktaemin <kwak9898@github.com>
2022-09-16 12:22:38 +0900

    update 관련 controller 제거

--- src/domains/users/users.controller.ts
+++ src/domains/users/users.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { Public } from "../../dacorators/skip-auth.decorator";
 import { User } from "./entities/user.entity";
@@ -36,11 +28,6 @@ export class UsersController {
   async createUser(@Body() user: User): Promise<User> {
     await this.usersService.createUser(user);
     return user;
-  }
-
-  @Patch("update-user")
-  async updateByUser(@Body() user: User): Promise<User> {
-    return await this.usersService.updateByUser(user);
   }
 
   @Delete(":userId")
