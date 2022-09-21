bb67284424326dee40ca75ab51860c1b211b1bce
kwaktaemin <kwak9898@github.com>
2022-09-21 17:12:05 +0900

    ,

--- src/domains/users/users.controller.ts
+++ src/domains/users/users.controller.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { Controller, Delete, Get, Param, UseGuards } from "@nestjs/common";
+=======
+import { Controller, Delete, Get, Param } from "@nestjs/common";
+>>>>>>> d83053e6 (사용하지 않은 함수 및 컨트롤러 정리 / 중복 아이디 처리)
 import { UsersService } from "./users.service";
 import { User } from "./entities/user.entity";
 import { JwtAuthGuard } from "../../guards/jwt-auth.guard";
@@ -19,7 +23,17 @@ export class UsersController {
   }
 
   @Delete(":userId")
+<<<<<<< HEAD
   async deleteByUser(@Param("userId") userId: string): Promise<void> {
     return await this.usersService.remove(userId);
+=======
+  async remove(@Param() userId: string): Promise<void> {
+    return this.usersService.remove(userId);
+  }
+
+  @Delete(":userId")
+  async deleteByUser(@Param() userId: string) {
+    return await this.usersService.deleteByUser(userId);
+>>>>>>> d83053e6 (사용하지 않은 함수 및 컨트롤러 정리 / 중복 아이디 처리)
   }
 }
