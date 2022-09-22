aa703ba8f3a92360e2da5cb3e5dac58b6e714ad8
kwaktaemin <kwak9898@github.com>
2022-09-22 11:32:08 +0900

    create-user DTO 생성

---
+++ src/domains/users/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  // 영문 + 숫자 유효성 체크
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: "영어와 숫자의 조합으로 다시 시도해 주세요.",
+  })
+  userId: string;
+
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @MinLength(10)
+  @MaxLength(20)
+  // 영문 + 숫자 + 특수문자 유효성 체크
+  @Matches(/^[a-zA-Z0-9`~!@#$%^&*()-_=+]*$/)
+  password: string;
+}
--- src/domains/users/dto/register.dto.ts
+++
@@ -1,5 +0,0 @@
-export class RegisterDto {
-  userId: string;
-  username: string;
-  password: string;
-}
