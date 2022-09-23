1ea69f92c09d17b5d8cbb639602c634bde84e810
kwaktaemin <kwak9898@github.com>
2022-09-23 14:23:15 +0900

    유저 역할 관련 Repository

---
+++ src/repositories/role.repository.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { DataSource, Repository } from "typeorm";
+import { User } from "../domains/users/entities/user.entity";
+import { Roles } from "../enum/roles.enum";
+import { UsersService } from "../domains/users/users.service";
+
+@Injectable()
+class RoleRepository extends Repository<User> {
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly userService: UsersService
+  ) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  // 유저 역할 조회
+  async getRoleByUser(userId: string, roleName: string): Promise<User> {
+    const user = await this.findOne({ where: { userId, roleName } });
+    return user;
+  }
+
+  // 유저 역할 수정
+  async updateRoleByUser(userId: string, role: Roles): Promise<User> {
+    const user = await this.userService.getUserById(userId);
+
+    user.roleName = role;
+    user.updatedAt = new Date();
+    await this.save(user);
+
+    return user;
+  }
+
+  // 유저 역할 삭제
+  async getDelete(userId: string, roleName: string): Promise<void> {
+    const user = await this.userService.getUserById(userId);
+
+    if (user) {
+      await this.delete(roleName);
+    } else {
+      throw new NotFoundException("존재하지 않는 유저입니다.");
+    }
+  }
+}
