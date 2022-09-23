95f8e54965535300a1c437a3f89394230c895373
kwaktaemin <kwak9898@github.com>
2022-09-23 18:08:43 +0900

    코드 수정

--- src/domains/users/users.module.ts
+++ src/domains/users/users.module.ts
@@ -4,9 +4,23 @@ import { UsersController } from "./users.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { UserRepository } from "./user.repository";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get("JWT_ACCESS_TOKEN_SECRET"),
+        signOptions: {
+          expiresIn: `${configService.get("JWT_ACCESS_TOKEN_EXPIRATION_TIME")}`,
+        },
+      }),
+    }),
+  ],
   providers: [UsersService, UserRepository],
   controllers: [UsersController],
   exports: [UsersService, UserRepository],
