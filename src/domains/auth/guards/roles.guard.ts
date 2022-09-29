0a7b2dd988583e558057081e80af1eba2c1bf545
kwaktaemin <kwak9898@github.com>
2022-09-29 10:41:24 +0900

    rols Guard 생성

---
+++ src/domains/auth/guards/roles.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { ROLES_KEY } from "../../../dacorators/role.decorator";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requireRole = this.reflector.getAllAndOverride<string>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    console.log(requireRole);
+    if (!requireRole) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+    console.log(user);
+    return requireRole == user.roleName;
+  }
+}
