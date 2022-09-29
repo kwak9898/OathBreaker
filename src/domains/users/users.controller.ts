72a9e6f8771e0aa234b22b5e3b42a3a487d68126
kwaktaemin <kwak9898@github.com>
2022-09-29 12:31:59 +0900

    유저 API 관리자 권한 부여

--- src/domains/users/users.controller.ts
+++ src/domains/users/users.controller.ts
@@ -1,13 +1,22 @@
 import { UsersService } from "./users.service";
-import { Body, Controller, Delete, Get, Param, Patch } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  UseGuards,
+} from "@nestjs/common";
 import { User } from "./entities/user.entity";
 import { ApiTags } from "@nestjs/swagger";
 import { Roles } from "../../dacorators/role.decorator";
 import { Role } from "../../enum/role.enum";
+import { RolesGuard } from "../auth/guards/roles.guard";
 
 @Controller("users")
-@Roles(Role.admin)
 @ApiTags("USERS")
+@UseGuards(RolesGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -19,12 +28,14 @@ export class UsersController {
   }
 
   // 특정 유저 조회
+  @Roles(Role.admin)
   @Get(":userId")
   async getUserById(@Param("userId") userId: string): Promise<User> {
     return this.usersService.getUserById(userId);
   }
 
   // 특정 유저 수정
+  @Roles(Role.admin)
   @Patch(":userId")
   async updateUser(
     @Param("userId") userId: string,
@@ -34,6 +45,7 @@ export class UsersController {
   }
 
   // 특정 유저 삭제
+  @Roles(Role.admin)
   @Delete(":userId")
   async deleteUser(@Param("userId") userId: string): Promise<void> {
     return this.usersService.deleteUser(userId);
