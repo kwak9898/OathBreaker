0b8328787f9e556a12677516d468d776e1379854
kwaktaemin <kwak9898@github.com>
2022-10-25 16:25:35 +0900

    역할 DB 생성

--- src/domains/roles/roles.module.ts
+++ src/domains/roles/roles.module.ts
@@ -1,37 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { RolesService } from "./roles.service";
 import { RolesController } from "./roles.controller";
-import { PassportModule } from "@nestjs/passport";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { JwtModule } from "@nestjs/jwt";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "../users/entities/user.entity";
-import { UsersModule } from "../users/users.module";
-import { AuthModule } from "../auth/auth.module";
-import { UsersService } from "../users/users.service";
-import { RoleRepository } from "./role.repository";
-import { UserRepository } from "../users/user.repository";
+import { RoleEntity } from "./entities/role.entity";
 
 @Module({
-  imports: [
-    UsersModule,
-    AuthModule,
-    PassportModule,
-    ConfigModule,
-    TypeOrmModule.forFeature([User]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get("JWT_ACCESS_TOKEN_SECRET"),
-        signOptions: {
-          expiresIn: `${configService.get("JWT_ACCESS_TOKEN_EXPIRATION_TIME")}`,
-        },
-      }),
-    }),
-  ],
-  providers: [RolesService, UsersService, RoleRepository, UserRepository],
-  exports: [RolesService, JwtModule],
+  imports: [TypeOrmModule.forFeature([RoleEntity])],
+  providers: [RolesService],
   controllers: [RolesController],
 })
-export class RolesModule {}
+export class LogsModule {}
