50fbe967d49b2561045216bcbe4ffbc465e22def
joel <joel.heo@mergerity.com>
2022-10-25 17:50:55 +0900

    fix: change docker-deploy for build stage target

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,4 @@
-docker build -t pumpkin-api .
+docker build -t --target production-stage pumpkin-api .
 docker stop pumpkin-api
 docker rm pumpkin-api
 docker run -d -p 8090:8090 --name pumpkin-api --rm pumpkin-api
