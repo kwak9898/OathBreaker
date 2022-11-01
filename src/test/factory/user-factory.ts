00e6e673c1b73fb8e7006523041a0ee1a14147e9
kwaktaemin <ian.kwak@mergerity.com>
2022-11-01 16:23:17 +0900

    유저 생성 및 리프레쉬 토큰 관련 함수 생성

--- src/test/factory/user-factory.ts
+++ src/test/factory/user-factory.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { User } from "../../domains/users/entities/user.entity";
 import { UserRepository } from "../../domains/users/user.repository";
 import { Role } from "../../domains/roles/enum/role.enum";
+import * as bcrypt from "bcryptjs";
 
 @Injectable()
 export class UserFactory {
@@ -22,6 +23,18 @@ export class UserFactory {
     user.userId = "manager1";
     user.username = "testManager";
     user.password = "password123@";
+    user.team = "운영";
+    user.roleName = Role.manager;
+    return this.repository.getUserById(
+      (await this.repository.save(user)).userId
+    );
+  }
+
+  async createTestUser() {
+    const user = new User();
+    user.userId = "testuser123";
+    user.username = "tester";
+    user.password = await bcrypt.hash("password123@", 12);
     user.team = "운영";
     user.roleName = Role.manager;
     return this.repository.getUserById(
